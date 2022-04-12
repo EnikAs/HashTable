3361c0a6ae6878648e265d608fddd1d6f9d23fb0
EnikAs <enikeev0323@gmail.com>
2022-04-12 18:33:02 +0300

    initial commit

---
+++ listfunc.h
@@ -0,0 +1,113 @@
+#ifndef LISTFUNC_H_INCLUDED
+#define LISTFUNC_H_INCLUDED
+
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <string.h>
+#include <stdlib.h>
+#include <cassert>
+
+typedef int list_t;
+
+const int LIST_IS_FULL = 1488;
+
+#define OK_CHECK_ON
+
+#ifdef OK_CHECK_ON
+#define $ListOkCheck(list, error) ListOkCheck(list, error)
+#else
+#define $ListOkCheck(list, error)    
+#endif
+
+
+#define PRINT_LINE printf("I'm at line %d at function %s\n", __LINE__, __func__);
+
+enum for_fun
+{
+    OK = 1337,
+    NOT_OK
+};
+
+enum open_file_check
+{
+    FILE_NOT_OPENED = 123,
+    FILE_OPENED
+};  
+
+enum calloc_check
+{
+    INCORRECT_CALLOC = 228,
+    CORRECT_CALLOC,
+    INCORRECT_REALLOC,
+    CORRECT_REALLOC
+};
+
+enum logic_check_key
+{
+    PREV_CHECK = 322,
+    NEXT_CHECK,
+    INCORRECT_KEY
+};
+
+enum ok_check
+{
+    LOGIC_NEXT_ERROR = 1488,
+    LOGIC_PREV_ERROR,
+    LOGIC_IS_OK,
+    TAIL_NEXT_ERROR,
+    HEAD_PREV_ERROR,
+    SIZE_ERROR,
+    LOOP_ERROR
+};
+
+struct ListElem 
+{
+    list_t data = 0;
+    int next = -1;
+    int prev = -1;
+};
+
+struct List
+{
+    ListElem* lstelem;
+    int size = 0;
+    int capacity = 0;
+    int free = 0;
+    int head = 0;
+    int tail = 0;
+    bool sorted = false;
+};
+
+
+int         ListInit                (List* list, int elem_num);
+
+void        SetListElem             (List* list, int elem_num, list_t data, int next, int prev);
+
+int         ListInsertTail          (List* list, list_t value);
+
+int         ListInsertHead          (List* list, list_t value);
+
+int         ListInsert              (List* list, list_t value, int pos_init = -1);
+
+int         ListDelete              (List* list, int pos_init);
+
+int         ListResize              (List* list);
+
+int         SlowSlowLinearaise      (List* list);
+
+int         SlowSlowLogicIntoPhys   (List* list, int logic);
+
+int         logic_error_check       (List* list, int tmp_pos, int key);
+
+int         ListOkCheck             (List* list, int* error);
+
+int         EasyDump                (List* list, FILE* log_file = stdout);
+
+int         ListDtor                (List* list);
+
+int         ListDump                (List* list, int error = -1);
+
+int         ListHtmlDump            (List* list);
+
+#endif
